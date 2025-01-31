7518a0493aa513ab797172c493a81ab876480b0a
Andrew Sas <andrewfsas@gmail.com>
2025-01-31 14:05:10 -0500

    Add q4

--- Week04/lab_week4.py
+++ Week04/lab_week4.py
@@ -81,6 +81,9 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+# Lab4 Q4
+belt = []
+
 # Lab4 Q1 - Defining the Monster Power
 monster_powers = {
     "Fire Magic": 2,
