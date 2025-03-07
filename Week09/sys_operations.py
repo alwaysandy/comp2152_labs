5333aa3b50bc93a321680981444b07660dea4906
alwaysandy <andrewfsas@gmail.com>
2025-03-07 13:42:10 -0500

    Lab 9 work

---
+++ Week09/sys_operations.py
@@ -0,0 +1,12 @@
+import platform
+import socket
+import os
+
+print(f"Machine Type: {platform.machine()}\n")
+print(f"CPU Architecture: {platform.architecture()}\n")
+
+socket.setdefaulttimeout(50)
+print(f"Socket Default Timeout: {socket.getdefaulttimeout()}\n")
+
+print(f"OS Type: {os.name}\n")
+print(f"OS name: {platform.system()}\n")
